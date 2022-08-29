91a43fcf7bccec50326fdc7a72aa8bd16b442b3c
achalgoel <goelachal4@gmail.com>
2022-08-29 13:04:40 -0600

    Final

--- src/Car.java
+++ src/Car.java
@@ -1,21 +1,36 @@
 public class Car {
     private double gasTankCapacity;
     private double gasTankLevel;
+    private double totalMiles;
 
     public Car() {
         gasTankCapacity = 15;
         gasTankLevel = 15;
+        totalMiles = 0;
     }
 
-    public Car(double CapacityParam, double LevelParam) {
+    public Car(double CapacityParam, double LevelParam, double milesParam) {
         gasTankLevel = LevelParam;
         gasTankCapacity = CapacityParam;
+        totalMiles = milesParam;
     }
 
     public void useGas(double gasUsed) {
         gasTankLevel = gasTankLevel - gasUsed;
     }
 
+
+    public void fillGas(double gasFilled) {
+        gasTankLevel = gasTankLevel + gasFilled;
+    }
+
+    public double carMiles() {
+        return totalMiles;
+    }
+
+    public double gasLeft() {
+        return gasTankLevel;
+    }
     public String toString() {
         return "Gas info:\nCapacity: " + gasTankCapacity + " Level: " + gasTankLevel;
     }
--- src/CarTester.java
+++ src/CarTester.java
@@ -1,13 +1,22 @@
 public class CarTester {
     public static void main(String Args[]) {
         Car car1 = new Car();
+        Car car2 = new Car(20,7,200000);
 
-        System.out.println(car1);
-
-        Car car2 = new Car(20,7);
-        System.out.println(car2);
+        System.out.println("Car 1:\n" + car1);
         car1.useGas(4);
-        System.out.println(car1);
+        double remainingFuel;
+        double miles;
+
+        System.out.println("Car 1 after a ride:\n" + car1);
+        remainingFuel = car1.gasLeft();
+        System.out.println("Gas remaining: "+ remainingFuel);
+        car1.fillGas(4);
+        remainingFuel = car1.gasLeft();
+        System.out.println("Gas after refuel: " + remainingFuel);
+        miles = car2.carMiles();
+        System.out.println("Total miles: " + miles);
+
 
     }
 }
